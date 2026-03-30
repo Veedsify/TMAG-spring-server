185a81ed3d223240b875c7cb24fa8d3306f11ff9
Dike Wisdom <dikewisdom787@gmail.com>
2026-03-30 01:39:04 +0100

    fix: Change ID generation strategy to IDENTITY for BaseEntity

--- src/main/java/com/TravelMedicineAdvisory/Server/core/base/BaseEntity.java
+++ src/main/java/com/TravelMedicineAdvisory/Server/core/base/BaseEntity.java
@@ -10,14 +10,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.SequenceGenerator;
 
 @MappedSuperclass
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "entity_seq")
-    @SequenceGenerator(name = "entity_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @CreationTimestamp
