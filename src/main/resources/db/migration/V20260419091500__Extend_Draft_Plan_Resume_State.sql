b29b0edcb53ddf73d1c5425fad32c1e3a1e52e4b
Dike Wisdom <dikewisdom787@gmail.com>
2026-04-20 16:52:09 +0100

    Consolidate Flyway migrations into single initial script
    Remove individual migration files and merge draft_plans schema
    into the main initialization migration. Also fix property syntax
    error in application.properties where `{` should be `${`.

--- src/main/resources/db/migration/V20260419091500__Extend_Draft_Plan_Resume_State.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE draft_plans
-    ADD COLUMN show_verify BOOLEAN DEFAULT FALSE;
-
-ALTER TABLE draft_plans
-    ADD COLUMN show_intro BOOLEAN DEFAULT TRUE;
-
-ALTER TABLE draft_plans
-    ADD COLUMN risk_consent_given BOOLEAN DEFAULT FALSE;
