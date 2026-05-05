201f6424cd615471a1e454d32d5ebc506018899e
Dike Wisdom <dikewisdom787@gmail.com>
2026-05-05 13:46:59 +0100

    refactor: Remove SQL migration for nullable columns in doctor applications

--- src/main/resources/db/migration/V20260504000001__doctor_application_upload_columns_nullable.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE doctor_applications
-    ALTER COLUMN medical_license_url DROP NOT NULL,
-    ALTER COLUMN identity_document_url DROP NOT NULL,
-    ALTER COLUMN cv_or_profile_url DROP NOT NULL;
